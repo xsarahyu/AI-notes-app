f6d282494f9401dc7518f6a172699ddeb5da7419
Sarah <xsarahyu@gmail.com>
2024-05-02 13:34:51 -0400

    Add Mongo URI to server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
